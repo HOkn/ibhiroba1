fee55bbea5c985f91ece148654cbf68d455195d9
Haru Okaniwa <haruokny@gmail.com>
2015-04-04 04:23:12 +0900

    root to register

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       resources :answers
   end
 
-  root to: 'questions#index'
+  root to: 'registrations#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
