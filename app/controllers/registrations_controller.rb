c4bf0ca5c1dc924d9f987e59a5ab69fab1f95747
Haru Okaniwa <haruokny@gmail.com>
2015-04-07 10:08:01 +0900

    no validation of status

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,13 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(params_user)
-
     if @user.save
+      raise
+
       login(:email, :password)
       redirect_to questions_path
     else
+      raise
       render :new
     end
   end
