d206e27bfb90d7a91f3addd4ef7acad1ba11eec0
Haru Okaniwa <haruokny@gmail.com>
2015-04-07 12:23:16 +0900

    answer longer possible

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  validates :content, presence: true, length: { in: 5..1000 }
+  validates :content, presence: true, length: { in: 5..3000 }
 end
