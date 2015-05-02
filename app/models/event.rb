63a6a8ccf4ba69c46be549f9027c8c6e16c688c4
Haru Okaniwa <haruokny@gmail.com>
2015-05-02 10:20:51 +0900

    [add] validation [modify] 自己紹介

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+  validates :session, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :email, presence: true, length: { maximum: 40 }
+  validates :school, presence: true, length: { maximum: 40 }
+  validates :comment, presence: true, length: { maximum: 2000 }
 end
